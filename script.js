ed56a46967d42ba97f626e1bc4a98e1625f7fc1c
duckuments <duckuments@gmail.com>
2025-11-03 08:51:03 +0330

    refactor: 💡 make list elements with js

--- script.js
+++ script.js
@@ -1,18 +1,53 @@
 let data = [];
 
+function createItem(text, status) {
+  // <li>
+  const li = document.createElement('li')
+  li.className = "flex flex-row gap-3 p-3 shadow-sm items-center justify-between"
+
+  // left box container
+  const left = document.createElement('div')
+  left.className = "flex flex-row gap-2 items-center"
+
+  // checkbox
+  const checkbox = document.createElement('input')
+  checkbox.type = "checkbox"
+  checkbox.checked = status
+  checkbox.className = "checkbox"
+
+  // text
+  const p = document.createElement('p')
+  p.textContent = text
+
+  // add checkbox + text into left container
+  left.appendChild(checkbox)
+  left.appendChild(p)
+
+  // delete button
+  const btn = document.createElement('button')
+  btn.type = "button"
+  btn.className = "btn btn-small btn-error"
+
+  // icon
+  const icon = document.createElement('i')
+  icon.className = "bx bx-trash"
+
+  btn.appendChild(icon)
+
+  // put left container + button inside <li>
+  li.appendChild(left)
+  li.appendChild(btn)
+
+  return li
+}
+
 
 function updateTasks() {
-  let list = document.getElementById("list");
-  list.innerHTML = ""
+  const list = document.querySelector('ul');
+  list.innerHTML = "";
 
   data.forEach(item => {
-    let li = document.createElement("li")
-    li.className = "flex flex-row gap-3 p-3 shadow-sm items-center justify-between"
-    console.log(item)
-
-    li.innerHTML = item.text
-
-    list.appendChild(li)
+    list.appendChild(createItem(item.text, item.status))
   })
 
   console.log("update run", data)
